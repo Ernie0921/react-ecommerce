d7378d9ba8015999bbf5ed34ee6b8cbea3e24391
Ernie0921 <ernesto.carrillo10@icloud.com>
2021-04-10 14:29:30 -0400

    added staic-data file

---
+++ src/static-data.js
@@ -0,0 +1,17 @@
+let items = [
+  {
+    id: 0,
+    name: "Apple ipad",
+    description: "An ipad like no other.",
+    price: 329.00
+  },
+  {
+    id: 1,
+    name: "Apple ipad Pro",
+    description: "Even more expensive than the ipad",
+    price: 729.00
+
+  }
+]
+
+export {items};
